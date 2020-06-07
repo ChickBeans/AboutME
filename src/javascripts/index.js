c9746f63cc8c49a73accc94ccf7aab63c954bfed
kusaka <kusaka0110ryu@gmail.com>
2020-06-07 23:10:37 +0900

    layout, mobileMenuBtnAnimation ,textAnimation

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,3 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '../stylesheets/vendors/bootstrap-reboot.css'
 import '../stylesheets/style.scss';
 import './main.js';
 
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,1 +1,50 @@
-console.log('fight');
+document.addEventListener('DOMContentLoaded', function() {
+
+const globalContainer = document.getElementById("global-container");
+const mobileMenuBtn = document.getElementById("header__mobile-menu__button");
+
+const _toggleClass = () => {
+  globalContainer.classList.toggle("menu-open");
+};
+
+mobileMenuBtn.addEventListener("click", _toggleClass);
+
+const main = document.getElementById("main");
+
+const _removeClass = () => {
+  globalContainer.classList.remove("menu-open");
+};
+
+main.addEventListener("click", function () {
+  globalContainer.classList.remove("menu-open");
+});
+
+const mobileItems = document.querySelectorAll(".mobile-menu__item");
+mobileItems.forEach((item) => {
+  item.addEventListener("click", _removeClass);
+});
+
+const ta = new TextAnimation('#ani');
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    ta.animate();
+});
+})
+
+class TextAnimation {
+    constructor(el) {
+        this.el = document.querySelector(el);
+        this.chars = this.el.innerHTML.trim().split("");
+        this.el.innerHTML = this._splitText();
+    }
+    _splitText() {
+        return this.chars.reduce((accu, curr) => {
+            // 半角スペースの置換
+            curr = curr.replace(/\s+/, "&nbsp;");
+            return `${accu}<span class="char">${curr}</span>`;
+          }, "");
+    }
+    animate() {
+        this.el.classList.toggle('inview');
+    }
+}
