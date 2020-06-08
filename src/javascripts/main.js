c86f0b0d0c7062a5290f24cbf8cf98e0ea636860
kusaka <kusaka0110ryu@gmail.com>
2020-06-08 23:51:58 +0900

    add slider

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -18,6 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   mobileMenuItems.removeCls("click", "menu-open");
 
+  // *******Slide Show***********
+  const images = ['../images/airbnb-img.png', '../images/iSara-img.png', '../images/airbnb-img.png'];
+  let current = 0;
+
+  const changeImage = (num) => {
+    if (current + num >= 0 && current + num < images.length) {
+      current += num;
+      document.querySelector('.deliverbles__main-image').src = images[current];
+      pageNum();
+    }
+  };
+
+const pageNum = () => {
+  document.getElementById('page').textContent = `${current + 1}/${images.length}`;
+}
+
+pageNum();
+
+  const prev = document.getElementById('prev');
+  console.log('prev: ', prev);
+  const next = document.getElementById('next');
+  prev.addEventListener('click', function() {
+    changeImage(-1);
+  });
+  next.addEventListener('click', function() {
+    changeImage(1);
+  });
+  // next.addEventListener('click', changeImage(1));
+  
+
   // ********observer********
   const cb = function (el, isIntersecting) {
     if (isIntersecting) {
